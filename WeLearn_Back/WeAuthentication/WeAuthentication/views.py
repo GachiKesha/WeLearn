479418a87cce375d1661fbaee3ee783cc3bc0361
sommeonne <brukaliuk.serhii@lll.kpi.ua>
2023-11-09 19:28:11 +0200

    commit

--- WeLearn_Back/WeAuthentication/WeAuthentication/views.py
+++ WeLearn_Back/WeAuthentication/WeAuthentication/views.py
@@ -5,27 +5,27 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
+from WeAuthentication.welearn.models import CustomUser
 from rest_framework.authtoken.models import Token
 
-from .serializers import UserSerializer
+from WeAuthentication.welearn.serializers import CustomUserSerializer
 
 
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(User, username=request.data['username'])
+    user = get_object_or_404(CustomUser, username=request.data['username'])
     if not user.check_password(request.data['password']):
         return Response({"detail":"Not found."},status=status.HTTP_404_NOT_FOUND)
     token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
+    serializer = CustomUserSerializer(instance=user)
     return Response({"token": token.key, "user": serializer.data})
 
 @api_view(['POST'])
 def signup(request):
-    serializer = UserSerializer(data=request.data)
+    serializer = CustomUserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        user = User.objects.get(username=request.data['username'])
+        user = CustomUser.objects.get(username=request.data['username'])
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
--- WeLearn_Back/WeAuthentication/welearn/models.py
+++ WeLearn_Back/WeAuthentication/welearn/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(models.Model):
+    username = models.CharField(unique=True, max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    known_languages = models.CharField(max_length=100, blank=True, null=True)
+    desired_language = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
