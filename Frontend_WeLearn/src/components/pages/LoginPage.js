c6650d5b90e33cbf31b5f634246dae93f4c92de1
ekslitri <122708869+ekslitri@users.noreply.github.com>
2023-11-29 20:46:21 +0200

    Update LoginPage.js

--- Frontend_WeLearn/src/components/pages/LoginPage.js
+++ Frontend_WeLearn/src/components/pages/LoginPage.js
@@ -6,43 +6,39 @@ import './style.css';
 import './login.css';
 import icon from './icon.png';
 
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const validateFields = () => {
+    let isValid = true;
+
+    if (!email) {
+      setEmailError('Please enter your email.');
+      isValid = false;
+    } else if (!emailPattern.test(email)) {
+      setEmailError('Please enter a valid email address.');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+
+    if (!password) {
+      setPasswordError('Please enter your password.');
+      isValid = false;
+    } else {
+      setPasswordError('');
+    }
+    return isValid;
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const onSubmit = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:8000/login/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: email,
-          password: password,
-        }),
-      });
-
-      if (!response.ok) {
-        // Handle error, display error message to the user
-        console.error('Login failed');
-        return;
-      }
-
-      const data = await response.json();
-      // Handle successful login, store token or user data in your app
-
-      console.log('Login successful', data);
-    } catch (error) {
-      console.error('Error during login:', error);
+  const onSubmit = () => {
+    if (validateFields()) {
+      // continue with your submit logic
+      console.log('Validated. Submitting...');
     }
   };
 
@@ -54,47 +50,47 @@ function LoginPage() {
       </head>
       <body>
         <Header />
-        <div className="container flex">
+        <div className="container1 flex">
           <img src={icon} alt="Icon" />
           <div className="page flex">
-            <div className="post">
-              <form onSubmit={onSubmit}>
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  placeholder="mail@abc.com"
-                  required
-                  value={email}
-                  onChange={handleEmailChange}
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  placeholder="*****************"
-                  required
-                  value={password}
-                  onChange={handlePasswordChange}
-                />
-                <label className="checkbox-container">
-                  Remember Me
-                  <input type="checkbox" id="remember" name="remember" />
-                  <div className="checkmark"></div>
-                  <a href="##" className="forgot-link">
-                    Forgot Password?
-                  </a>
-                </label>
-                <div className="link">
-                  <button type="submit" className="login">
-                    Login
-                  </button>
-                  <p>
-                    Not Registered Yet?{' '}
-                    <Link to="/registration" className="createAcc">
-                      Create account
-                    </Link>
-                  </p>
-                </div>
-              </form>
+            <div className="post1">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                placeholder="mail@abc.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              {emailError && <p className="error-message">{emailError}</p>}
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="*****************"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              {passwordError && <p className="error-message">{passwordError}</p>}
+              <label className="checkbox-container">
+                Remember Me
+                <input type="checkbox" id="remember" name="remember" />
+                <div className="checkmark"></div>
+                <a href="##" className="forgot-link">
+                  Forgot Password?
+                </a>
+              </label>
+              <div className="link">
+                <button type="button" onClick={onSubmit} className="login">
+                  Login
+                </button>
+                <p>
+                  Not Registered Yet?{' '}
+                  <Link to="/registration" className="createAcc">
+                    Create account
+                  </Link>
+                </p>
+              </div>
             </div>
           </div>
         </div>
