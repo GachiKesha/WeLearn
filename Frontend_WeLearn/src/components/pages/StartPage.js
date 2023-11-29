4bdac9e680af51c0b13423c57028c55092069b46
ekslitri <122708869+ekslitri@users.noreply.github.com>
2023-11-29 21:00:34 +0200

    Update StartPage.js

--- Frontend_WeLearn/src/components/pages/StartPage.js
+++ Frontend_WeLearn/src/components/pages/StartPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 import Header from '../common/Header';
 import Support from '../common/Support';
 import './style.css';
@@ -8,11 +8,33 @@ import settingLogo from './settingLogo.png';
 
 function StartPage() {
   const navigate = useNavigate();
+  const [fieldValue, setFieldValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [startButtonClicked, setStartButtonClicked] = useState(false);
 
   const onStart = () => {
-    console.log('Start button clicked');
-    // Redirect to /videocallPage
-    navigate('/videocall');
+    setStartButtonClicked(true); 
+    const trimmedFieldValue = fieldValue.trim();
+    const langChoice = document.querySelector('.lang-choice').value;
+    const desLangChoice = document.querySelector('.des-lang-choice').value;
+  
+    if (trimmedFieldValue === '' && (langChoice === desLangChoice)) {
+      setErrorMessage('Please fill in the field before starting and choose different languages.');
+    } else if (trimmedFieldValue === '') {
+      setErrorMessage('Please fill in the field before starting.');
+    } else if (langChoice === desLangChoice) {
+      setErrorMessage('Please choose different languages.');
+    } else {
+      console.log('Start button clicked');
+      // redirect to /videocallPage
+      navigate('/videocall');
+    }
+  };
+
+  const handleFieldChange = (event) => {
+    setFieldValue(event.target.value);
+    // clear error message when the field changes
+    setErrorMessage('');
   };
 
   return (
@@ -26,8 +48,11 @@ function StartPage() {
       <div className="container flex">
         <div className="page flex">
           <div className="post">
-            <label htmlFor="Name">Name</label>
-            <input type="Name" placeholder="Name" required />
+          <label htmlFor="Name">Name</label>
+      <input type="text" id="Name" placeholder="Name"required value={fieldValue} onChange={handleFieldChange} />
+      {/* display error message*/}
+      {startButtonClicked && fieldValue === '' && (
+        <div className="error-message">Please enter your name.</div>)}
             <label htmlFor="lang-choice">Language to Learn</label>
             <select className="lang-choice">
               <option>English</option>
@@ -38,8 +63,9 @@ function StartPage() {
               <option>Ukrainian</option>
               <option>English</option>
             </select>
+            {errorMessage && <div className="error-message">{errorMessage}</div>}
             <div className="link">
-              <button type="start" className="start" onClick={onStart}>
+              <button type="button" className="start" onClick={onStart}>
                 ▶
               </button>
             </div>
