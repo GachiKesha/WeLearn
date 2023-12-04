d5f29339a43899478ead2c63589d84f8fdeab7d4
GachiKesha <148109167+GachiKesha@users.noreply.github.com>
2023-12-05 01:28:58 +0200

    tilt (#8)
    * Revert "Update LoginPage.js"
    
    This reverts commit 707018d5e19179ede4191bf874b3b2c4ff0404d7.
    
    * Revert "Update StartPage.js"
    
    This reverts commit 4bdac9e680af51c0b13423c57028c55092069b46.
    
    * Update LoginPage.js
    
    * Revert "Revert "Update StartPage.js""
    
    This reverts commit d0528d6fed8f3feccd937462816e6058f2fa635a.
    
    * revert of revert of comit of some crap
    
    yamete kudasai

--- Frontend_WeLearn/src/components/pages/LoginPage.js
+++ Frontend_WeLearn/src/components/pages/LoginPage.js
@@ -6,97 +6,92 @@ import './style.css';
 import './login.css';
 import icon from './icon.png';
 
-const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
 
-  const validateFields = () => {
-    let isValid = true;
-
-    if (!email) {
-      setEmailError('Please enter your email.');
-      isValid = false;
-    } else if (!emailPattern.test(email)) {
-      setEmailError('Please enter a valid email address.');
-      isValid = false;
-    } else {
-      setEmailError('');
-    }
-
-    if (!password) {
-      setPasswordError('Please enter your password.');
-      isValid = false;
-    } else {
-      setPasswordError('');
-    }
-    return isValid;
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
 
-  const onSubmit = () => {
-    if (validateFields()) {
-      // continue with your submit logic
-      console.log('Validated. Submitting...');
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmit = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/login/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: email,
+          password: password,
+        }),
+      });
+
+      if (!response.ok) {
+        // Handle error, display error message to the user
+        console.error('Login failed');
+        return;
+      }
+
+      const data = await response.json();
+      // Handle successful login, store token or user data in your app
+
+      console.log('Login successful', data);
+    } catch (error) {
+      console.error('Error during login:', error);
     }
   };
 
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <title>WeLearn Login Page</title>
-      </head>
-      <body>
-        <Header />
+    <div>
         <div className="container1 flex">
           <img src={icon} alt="Icon" />
           <div className="page flex">
-            <div className="post1">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                placeholder="mail@abc.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              {emailError && <p className="error-message">{emailError}</p>}
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                placeholder="*****************"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              {passwordError && <p className="error-message">{passwordError}</p>}
-              <label className="checkbox-container">
-                Remember Me
-                <input type="checkbox" id="remember" name="remember" />
-                <div className="checkmark"></div>
-                <a href="##" className="forgot-link">
-                  Forgot Password?
-                </a>
-              </label>
-              <div className="link">
-                <button type="button" onClick={onSubmit} className="login">
-                  Login
-                </button>
-                <p>
-                  Not Registered Yet?{' '}
-                  <Link to="/registration" className="createAcc">
-                    Create account
-                  </Link>
-                </p>
-              </div>
+            <div className="post">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  placeholder="mail@abc.com"
+                  required
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  placeholder="*****************"
+                  required
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                <label className="checkbox-container">
+                  Remember Me
+                  <input type="checkbox" id="remember" name="remember" />
+                  <div className="checkmark"></div>
+                  <a href="##" className="forgot-link">
+                    Forgot Password?
+                  </a>
+                </label>
+                <div className="link">
+                  <button type="submit" onSubmit={onSubmit} className="login">
+                    Login
+                  </button>
+                  <p>
+                    Not Registered Yet?{' '}
+                    <Link to="/registration" className="createAcc">
+                      Create account
+                    </Link>
+                  </p>
+                </div>
             </div>
           </div>
         </div>
         <Support />
-      </body>
-    </html>
+    </div>
   );
 }
 
