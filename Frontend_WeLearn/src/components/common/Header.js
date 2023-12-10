08b43ee2b9bff39dd5f2ce9dd2b14917df3f172e
cloudysommme <112780513+cloudysommme@users.noreply.github.com>
2023-12-10 12:01:50 +0000

    header to menu

--- Frontend_WeLearn/src/components/common/Header.js
+++ Frontend_WeLearn/src/components/common/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Lora&family=Nunito+Sans:ital,opsz,wght@0,6..12,200;0,6..12,300;0,6..12,800;1,6..12,1000&display=swap"
         rel="stylesheet"/>
-      <h1 className={styles.h1Container}><Link to="/start">WeLearn</Link></h1>
+      <h1 className={styles.h1Container}><Link to="/menu">WeLearn</Link></h1>
     </div>
   );
 };
