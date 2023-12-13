f935b1a6731c7225f8420baa329cd721d5f28f9f
GachiKesha <stukalin.vlad@gmail.com>
2023-12-13 03:08:26 +0200

    url

--- WeLearn_Back/WeAuthentication/urls.py
+++ WeLearn_Back/WeAuthentication/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from welearn.views import login, signup, get_user, test_token, peer, ping_peer
+from welearn.views import login, signup,  test_token, peer, ping_peer
 
 urlpatterns = [
     path('login/', login),
     path('signup/', signup),
-    path('get_user/<int:id>/', get_user),
+    # path('get_user/<int:id>/', get_user),
     path('test_token/', test_token),
     path('peer/', peer),
     path('ping_peer/<int:id>/', ping_peer),
-    #path('close_peer/<int:id>/', close_peer),
-    #path('delete_peer/<int:id>/', delete_peer),
-   # path('peer_info/<int:id>/', peer_info),
+    # path('close_peer/<int:id>/', close_peer),
+    # path('delete_peer/<int:id>/', delete_peer),
+    # path('peer_info/<int:id>/', peer_info),
 ]
