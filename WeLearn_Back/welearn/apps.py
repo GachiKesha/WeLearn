7c2edc4d5be9bd314bd9e0e0f2dda20fea8b5261
cloudysommme <112780513+cloudysommme@users.noreply.github.com>
2023-12-15 18:30:00 +0000

    link to menu/log by shailushay

--- WeLearn_Back/welearn/apps.py
+++ WeLearn_Back/welearn/apps.py
@@ -6,6 +6,6 @@ class WelearnConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'welearn'
 
-    def ready(self):
-        with connection.cursor() as cursor:
-            cursor.execute("DELETE FROM sqlite_sequence WHERE name = 'welearn_peer'")
+    #def ready(self):
+        #with connection.cursor() as cursor:
+          #  cursor.execute("DELETE FROM sqlite_sequence WHERE name = 'welearn_peer'")
